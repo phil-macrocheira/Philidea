070a6ba620a4b9e61cfaadc01deb4ccc0d59bcbf
phil_macrocheira <philip.underwood2@gmail.com>
2023-12-02 15:32:59 -0600

    Add project files.

---
+++ TOTK.Website/Services/JsonService.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using TOTK.Website.Models;
+
+namespace TOTK.Website.Services
+{
+    public class JsonService
+    {
+        public JsonService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "data.json"); }
+        }
+
+        public IEnumerable<Weapon> GetWeapons()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Weapon[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
---
+++ TOTK.Website/Startup.cs
@@ -0,0 +1,51 @@
+﻿using TOTK.Website.Services;
+
+namespace TOTK.Website
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddHttpClient();
+            services.AddControllers();
+            services.AddTransient<JsonService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+            });
+        }
+    }
+}
