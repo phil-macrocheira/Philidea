1339ebe8f4444f39777313030025cc9bd1e5eb6c
benduckwdesign <benjamin@benjaminduckworth.com>
2024-02-25 21:17:36 -0500

    fix for bubble position when page is scrolled

--- Philidea.Website/wwwroot/acgc-se/js/toma-bubble.js
+++ Philidea.Website/wwwroot/acgc-se/js/toma-bubble.js
@@ -14,11 +14,11 @@
                 tooltipText.innerText = title;
                 var tooltip = document.querySelector('.ac-bubble');
                 tooltip.style.display = 'block';
-                var magicYnumber = document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().top;
-                var magicXnumber = document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().left;
+                var magicYnumber = document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().top + window.scrollY;
+                var magicXnumber = document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().left + window.scrollX;
                 var cellXSize = document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().right - document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().left;
                 cellXSize = cellXSize / 5;
-                tooltip.style.top = magicYnumber - (cellXSize / 2)  + ((Math.round((e.pageY - magicYnumber + (cellXSize / 2)) / cellXSize) * cellXSize )) + 'px'; // if grid is moved, adjust e.clientY +- y
+                tooltip.style.top = magicYnumber - (cellXSize / 2) + ((Math.round((e.pageY - magicYnumber + (cellXSize / 2)) / cellXSize) * cellXSize )) + 'px'; // if grid is moved, adjust e.clientY +- y
                 tooltip.style.left = magicXnumber + ((Math.round((e.pageX - magicXnumber - (cellXSize / 2)) / cellXSize) * cellXSize) + (cellXSize / 2)) + 'px'; // if grid is moved, adjust e.clientX +- x
             }
         });
