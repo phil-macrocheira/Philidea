79fa6357692ce778086d07bdde45268206819587
benduckwdesign <benjamin@benjaminduckworth.com>
2024-02-25 20:22:56 -0500

    fix bubble positioning

--- Philidea.Website/wwwroot/acgc-se/js/toma-bubble.js
+++ Philidea.Website/wwwroot/acgc-se/js/toma-bubble.js
@@ -2,21 +2,28 @@
     document.querySelectorAll('.inventory-slot').forEach(function (slot) {
         slot.addEventListener('mousemove', function (e) {
             var title = null;
-            var imgElement = slot.getElementsByTagName('img')[0];
+            var imgElements = slot.getElementsByTagName('img');
+            var imgElement = imgElements[0];
             if (imgElement && imgElement.hasAttribute('bubbletext')) {
                 title = slot.getElementsByTagName('img')[0].getAttribute('bubbletext');
+            } else {
+                return;
             }
             if (title !== null) {
                 var tooltipText = document.querySelector('.ac-bubble-content p');
                 tooltipText.innerText = title;
                 var tooltip = document.querySelector('.ac-bubble');
                 tooltip.style.display = 'block';
-                tooltip.style.top = ((Math.round((e.clientY - 32) / 64) * 64) + 36) + 'px'; // if grid is moved, adjust e.clientY +- y
-                tooltip.style.left = ((Math.round((e.clientX - 25 - 32) / 64) * 64) + 36) + 'px'; // if grid is moved, adjust e.clientX +- x
+                var magicYnumber = document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().top;
+                var magicXnumber = document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().left;
+                var cellXSize = document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().right - document.body.getElementsByClassName("inventory")[0].getBoundingClientRect().left;
+                cellXSize = cellXSize / 5;
+                tooltip.style.top = magicYnumber - (cellXSize / 2)  + ((Math.round((e.clientY - magicYnumber + (cellXSize / 2)) / cellXSize) * cellXSize )) + 'px'; // if grid is moved, adjust e.clientY +- y
+                tooltip.style.left = magicXnumber + ((Math.round((e.clientX - magicXnumber - (cellXSize / 2)) / cellXSize) * cellXSize) + (cellXSize / 2)) + 'px'; // if grid is moved, adjust e.clientX +- x
             }
         });
         slot.addEventListener('mouseout', function () {
-            var tooltip = document.querySelector('.bubble-tooltip');
+            var tooltip = document.querySelector('.ac-bubble');
             if (tooltip) {
                 tooltip.style.display = 'none';
             }
